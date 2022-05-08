19b4a8981b655dc08a9bc00e16e13a346eec2620
Adriana McGee <adriana@Adrianas-MBP.lan>
2022-05-07 21:32:16 -0400

    commit tree 9bb6de64db2ce6e1ed44c9ec26a0bcde23020414

---
+++ index.js
@@ -0,0 +1,34 @@
+function add(a, b){
+    return a + b;
+}
+
+function subtract(a, b){
+    return a - b;
+
+}
+
+function multiply(a, b){
+    return a * b;
+
+}
+
+function divide(a, b){
+    return a / b;
+}
+function increment(n){
+    return n += 1;
+}
+
+function decrement(n){
+    return n -= 1;
+}
+
+function makeInt(string){
+    string = parseInt(string, 10);
+    return string;
+}
+
+function preserveDecimal(string){
+    string = parseFloat(string);
+    return string;
+}
